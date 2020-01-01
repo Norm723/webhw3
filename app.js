a432bf46fde812093a485264165640273d673a52
norm723 <norm.twersky@gmail.com>
2020-01-01 11:57:45 +0200

    changes

--- app.js
+++ app.js
@@ -1,52 +1,73 @@
 $(document).ready(function() {
-    $("h1").html("Hello, World!");
+    /* $("h1").html("Hello, World!");
     $("#first").css("background","lightgrey");
     $("p").css("margin","30px");
     $("#first p").css("color","darkblue");
-    $("p:even").css("color","darkred");
-    $("form input, textarea, select").css({"margin":"20px 3px 0 10px", "font-size":"18px"})
+    $("p:even").css("color","darkred");*/
+    $("form input, textarea, select").css({"margin":"20px 3px 0 10px", "font-size":"18px"}) 
     
     
     
-    $("form input[type='submit']").click(function(){
+    $("form input[type='submit']").click(async function(){
       console.log("form submitted");
-      let fname = $('#fname').val().trim();
-      let lname = $('#lname').val().trim();
-      if(fname==''){fname='<span class="err">-empty-</span>'}
-      if(lname==''){lname='<span class="err">-empty-</span>'}
-      $('#output').html(      
-        'First name: ' + fname + '<br/>' +
-        'Last name: ' + lname + '<br/>' +
-        'Year: ' + $("input[name='year']:checked").val()
-      );
+      let item = $('#item').val().trim();
+      let quantity = $('#quantity').val().trim();
+      let email = $('#email').val().trim();
+      let clean = true;
+      if(item==''){item='<span class="err">required field</span>'; problem=false;}
+      if(quantity==''){quantity='<span class="err">required field</span>';problem=false;}
+      if(email==''){email='<span class="err">required field</span>';problem=false;}
+      let ship =$("input[name='ship']:checked").val();
+        if(ship == 'on') {
+          ship = 'Yes';
+        } else {
+          ship = 'No';
+        }
+      if (clean) {
+        $('#result').html(      
+          'Item Name: ' + item + '<br/>' +
+          'Quantity: ' + quantity + '<br/>' +
+          'Email: ' + email + '<br/>' +
+          'Message: ' + $('#message').val().trim() + '<br/>' +
+          'Color: ' + $("input[name='color']:checked").val() + '<br/>' +
+          'Size: ' + $("#size").children("option:selected").val() + '<br/>' +
+          'Free Shipping: ' + ship
+        );
+      }
       return false; // prevent default form action
     });
     
     $("form input").keypress(function(){
-      $('#output').html('');
+      console.log('in form');
+      $('#result').html('');
+    })
+
+    $("#message").focus(function(){
+      console.log('in textarea');
+      $(this).text('');
+    })
+
+    $("#message").blur(function(){
+      console.log('in textarea');
+      let val =  $(this).text();
+      console.log(val);
+      if(val == ''){
+        $(this).text('Message');
+      }
     })
     
-    $("p").click(function(){
+   /*  $("p").click(function(){
       console.log("p clicked")
       $(this).addClass('clicked');
       $(this).toggleClass('highlight');
     });
-    
-    $("#animate div").click(function(){
-      $(this).animate({
-        'top': Math.round(Math.random() * 100) + 'px',
-        'left': Math.round(Math.random() * 70) + '%',
-        'width': Math.round(Math.random() * 60 + 20) + '%',
-        'height': Math.round(Math.random() * 100 + 60) + 'px'
-      })
-    });
-    
-    $('h3').hover( function(){
+     */
+    /* $('h3').hover( function(){
         $(this).css('color','purple');
       }, function() {
         $(this).css('color','black');
-      }
-    );
+      } 
+    );*/
     
   // end jQuery document.ready  
   });
